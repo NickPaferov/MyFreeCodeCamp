90c93ba2589745616030568d0b72d540487098f2
Paferov Nikolai <paferov.nikolai@gmail.com>
2022-05-09 13:54:33 +0300

    ES6_21

---
+++ ES6/Use export to Share a Code Block.js
@@ -0,0 +1,18 @@
+/*export const uppercaseString = (string) => {
+    return string.toUpperCase();
+}*/
+
+/*
+export const lowercaseString = (string) => {
+    return string.toLowerCase()
+}*/
+
+const uppercaseString = (string) => {
+    return string.toUpperCase();
+}
+
+const lowercaseString = (string) => {
+    return string.toLowerCase()
+}
+
+export {uppercaseString, lowercaseString}
